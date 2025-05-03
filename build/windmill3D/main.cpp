4f07e407f87104b87c8f418c632e4f351dd2370c
Alteryxx <daedalusdivine69@gmail.com>
2025-05-04 03:13:55 +0800

    added build dependecies

---
+++ build/windmill3D/main.cpp
@@ -0,0 +1,20 @@
+#include <GL/glut.h>
+
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_TRIANGLES);
+        glColor3f(1.0f, 0.0f, 0.0f);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0.5f, -0.5f);
+        glVertex2f(0.0f, 0.5f);
+    glEnd();
+    glFlush();
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutCreateWindow("OpenGL Test");
+    glutDisplayFunc(display);
+    glutMainLoop();
+    return 0;
+}
